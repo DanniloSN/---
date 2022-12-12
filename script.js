9fffd6aab4f98f813c410eda871521d503b97ee4
Danilo de Souza Nunes <30867837+DanniloSN@users.noreply.github.com>
2022-12-12 18:37:38 -0300

    Added katakana array

--- script.js
+++ script.js
@@ -87,6 +87,95 @@ let hiragana = [
   { romanji: 'do', symbol: 'ど' },
 ]
 
+let katakana = [
+  { romanji: 'a', symbol: 'ア' },
+  { romanji: 'i', symbol: 'イ' },
+  { romanji: 'u', symbol: 'ウ' },
+  { romanji: 'e', symbol: 'エ' },
+  { romanji: 'o', symbol: 'オ' },
+  // K
+  { romanji: 'ka', symbol: 'カ' },
+  { romanji: 'ki', symbol: 'キ' },
+  { romanji: 'ku', symbol: 'ク' },
+  { romanji: 'ke', symbol: 'ケ' },
+  { romanji: 'ko', symbol: 'コ' },
+  // S
+  { romanji: 'sa', symbol: 'サ' },
+  { romanji: 'shi', symbol: 'シ' },
+  { romanji: 'su', symbol: 'ス' },
+  { romanji: 'se', symbol: 'セ' },
+  { romanji: 'so', symbol: 'ソ' },
+  // T
+  { romanji: 'ta', symbol: 'タ' },
+  { romanji: 'chi', symbol: 'チ' },
+  { romanji: 'tsu', symbol: 'ツ' },
+  { romanji: 'te', symbol: 'テ' },
+  { romanji: 'to', symbol: 'ト' },
+  // N
+  { romanji: 'na', symbol: 'ナ' },
+  { romanji: 'ni', symbol: 'ニ' },
+  { romanji: 'nu', symbol: 'ヌ' },
+  { romanji: 'ne', symbol: 'ネ' },
+  { romanji: 'no', symbol: 'ノ' },
+  // H
+  { romanji: 'ha', symbol: 'ハ' },
+  { romanji: 'hi', symbol: 'ヒ' },
+  { romanji: 'fu', symbol: 'フ' },
+  { romanji: 'he', symbol: 'ヘ' },
+  { romanji: 'ho', symbol: 'ホ' },
+  // M
+  { romanji: 'ma', symbol: 'マ' },
+  { romanji: 'mi', symbol: 'ミ' },
+  { romanji: 'mu', symbol: 'ム' },
+  { romanji: 'me', symbol: 'メ' },
+  { romanji: 'mo', symbol: 'モ' },
+  // Y
+  { romanji: 'ya', symbol: 'ヤ' },
+  { romanji: 'yu', symbol: 'ユ' },
+  { romanji: 'yo', symbol: 'ヨ' },
+  // R
+  { romanji: 'ra', symbol: 'ラ' },
+  { romanji: 'ri', symbol: 'リ' },
+  { romanji: 'ru', symbol: 'ル' },
+  { romanji: 're', symbol: 'レ' },
+  { romanji: 'ro', symbol: 'ロ' },
+  // W
+  { romanji: 'wa', symbol: 'ワ' },
+  { romanji: 'wo', symbol: 'ヲ' },
+  // N
+  { romanji: 'n', symbol: 'ン' },
+  // P
+  { romanji: 'pa', symbol: 'パ' },
+  { romanji: 'pi', symbol: 'ピ' },
+  { romanji: 'pu', symbol: 'プ' },
+  { romanji: 'pe', symbol: 'ペ' },
+  { romanji: 'po', symbol: 'ポ' },
+  // G
+  { romanji: 'ga', symbol: 'ガ' },
+  { romanji: 'gi', symbol: 'ギ' },
+  { romanji: 'gu', symbol: 'グ' },
+  { romanji: 'ge', symbol: 'ゲ' },
+  { romanji: 'go', symbol: 'ゴ' },
+  // B
+  { romanji: 'ba', symbol: 'バ' },
+  { romanji: 'bi', symbol: 'ビ' },
+  { romanji: 'bu', symbol: 'ブ' },
+  { romanji: 'be', symbol: 'ベ' },
+  { romanji: 'bo', symbol: 'ボ' },
+  // Z
+  { romanji: 'za', symbol: 'ザ' },
+  { romanji: 'zi', symbol: 'ジ' },
+  { romanji: 'zu', symbol: 'ズ' },
+  { romanji: 'ze', symbol: 'ゼ' },
+  { romanji: 'zo', symbol: 'ゾ' },
+  // D
+  { romanji: 'da', symbol: 'ダ' },
+  { romanji: 'di', symbol: 'ヂ' },
+  { romanji: 'du', symbol: 'ヅ' },
+  { romanji: 'de', symbol: 'デ' },
+  { romanji: 'do', symbol: 'ド' },
+]
+
 let actualHiragana = null
 let score = 0
 
