25bf9bc68b3173ef05c947e2c5c48b2efef6c57b
Danilo de Souza Nunes <30867837+DanniloSN@users.noreply.github.com>
2022-12-19 21:35:56 -0300

    Forgot the | 😅

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const hiragana = [
   // T
   { romanji: 'ta', symbol: 'た' },
   { romanji: 'chi', symbol: 'ち' },
-  { romanji: 'tsu', symbol: 'つっ' },
+  { romanji: 'tsu', symbol: 'つ|っ' },
   { romanji: 'te', symbol: 'て' },
   { romanji: 'to', symbol: 'と' },
   // N
@@ -108,7 +108,7 @@ const katakana = [
   // T
   { romanji: 'ta', symbol: 'タ' },
   { romanji: 'chi', symbol: 'チ' },
-  { romanji: 'tsu', symbol: 'ツッ' },
+  { romanji: 'tsu', symbol: 'ツ|ッ' },
   { romanji: 'te', symbol: 'テ' },
   { romanji: 'to', symbol: 'ト' },
   // N
